1cccf084e0749f673ae11f6a37462aa8cde1295b
r <chenray2003@gmail.com>
2024-04-07 15:32:00 -0400

    button on the ball, add scores

--- week3/funny/src/App.tsx
+++ week3/funny/src/App.tsx
@@ -14,7 +14,9 @@ const App: React.FC = () => {
   // they are just some variable, simple
   const [ballY, setBallY] = useState(200);  //set up the initial Y axies of the ball    y from the top?
   const [velocity, setVelocity] = useState(0); // starting velocity
-  const gravity = 0.45;
+  const [score, setScore] = useState(0);
+  const [lastscore, setLastScore] = useState(0);
+  const gravity = 0.35;
   const maxHeight = 800; //screen size
   const ballSize = 100;
 
@@ -26,9 +28,17 @@ const App: React.FC = () => {
         const newY = prevY + velocity;
         if (newY >= maxHeight - ballSize) {// Ball hits the ground
           setVelocity(0);
+          setScore(0)
+          if(score>0){
+            setLastScore(score)
+          }
           return maxHeight - ballSize; 
-        } else if (newY < 0) {// Ball hits the top
+        } else if (newY < 0) { // Ball hits the top
           setVelocity(0);
+          setScore(0)
+          if(score>0){
+            setLastScore(score)
+          }
           return 0;
         } else { // Ball is  in the air
           // refresh rate
@@ -48,22 +58,42 @@ const App: React.FC = () => {
 
   const handleButtonClick = () => {
     setVelocity(-10); // Apply upward force
+    setScore(prevScore=>prevScore+1)
   };
 
   const ballStyle: React.CSSProperties = {
     position: 'absolute',
     top: ballY,
     left: '50%',
-    transform: 'translateX(-50%)',
     backgroundColor: 'black',
     width: `${ballSize}px`,
     height: `${ballSize}px`,
     borderRadius: '50%',
   };
+  const buttonStyle: React.CSSProperties = {
+    position: 'absolute',
+    top: ballY+10,
+    left: '50.5%',
+    width: `${ballSize-20}px`,
+    height: `${ballSize-20}px`,
+    borderRadius: '50%',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    color : 'white',
+    zIndex: 1, //on the top of other things
+  };
 
+  
+  
   return (
-    <div className="App">
-      <button onClick={handleButtonClick}>Click me to make the ball go up!</button>
+    <div className='App'>
+      <section>
+        <h2>LAST SCORE: {lastscore}</h2>
+        <h2>CURRENT SCORE: {score}</h2>
+        <p>RULE: Score will be set back to 0 when the ball hits the ground or the ceiling</p>
+        <p>IMPORTANT: stop cheating using spacebar</p>
+
+      </section>
+      <button style={buttonStyle} onClick={handleButtonClick} >Click Meeee!</button>
       <div className="ball" style={ballStyle}></div>
     </div>
   );
